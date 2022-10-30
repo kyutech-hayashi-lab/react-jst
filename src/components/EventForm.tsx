96c64ef19263dc7f5186a348cbc08d817406622a
takemoto-eye-sales <t.mizuki@x-opg.com>
2022-10-30 11:37:44 +0900

    フォームの内容を更新

--- src/components/EventForm.tsx
+++ src/components/EventForm.tsx
@@ -9,12 +9,12 @@ export default function EventForm() {
         className="border rounded p-4 mt-4 mx-auto mw-100"
         style={{ width: '60em' }}
       >
-        <Form.Group className="mb-3" controlId="title">
+        <Form.Group className="mb-2" controlId="title">
           <Form.Label>イベントタイトル</Form.Label>
           <Form.Control placeholder="タイトルを入力してください" />
         </Form.Group>
 
-        <Form.Group className="mb-3" controlId="subscript">
+        <Form.Group className="mb-2" controlId="subscript">
           <Form.Label>イベント内容</Form.Label>
           <Form.Control
             as="textarea"
@@ -22,15 +22,23 @@ export default function EventForm() {
             placeholder="内容を入力してください"
           />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="place">
+        <Form.Group className="mb-2" controlId="place">
           <Form.Label>開催場所</Form.Label>
           <Form.Control placeholder="場所を入力してください" />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="date">
-          <Form.Label>開催日時</Form.Label>
-          <Form.Control type="date" placeholder="日時を選択してください" />
+        <Form.Group className="mb-2" controlId="date">
+          <Form.Label>開催日</Form.Label>
+          <Form.Control type="date" placeholder="開催日を選択してください" />
         </Form.Group>
-        <Form.Group controlId="formFile" className="mb-3">
+        <Form.Group className="mb-2" controlId="startTime">
+          <Form.Label>開始時刻</Form.Label>
+          <Form.Control type="time" placeholder="開始時刻を選択してください" />
+        </Form.Group>
+        <Form.Group className="mb-2" controlId="endTime">
+          <Form.Label>終了時刻</Form.Label>
+          <Form.Control type="time" placeholder="終了時刻を選択してください" />
+        </Form.Group>
+        <Form.Group className="mb-2" controlId="formFile">
           <Form.Label>イベントイメージ</Form.Label>
           <Form.Control type="file" />
         </Form.Group>
