31c44cdafef2dc124e8216ba1e96af0fcc2e3882
takemoto-eye-sales <t.mizuki@x-opg.com>
2023-08-20 17:00:41 +0900

    ヘッダー修正

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,9 +10,9 @@ export default function Header() {
   return (
     <div className="d-flex px-2 border-bottom border-2">
       <h3 className="mt-4 ms-5"><strong>ビーチクリーンクラウドサービス</strong></h3>
-      <div className="ms-auto d-flex mt-3">
-        <a href="/" className="text-dark">イベント主催者はこちら</a>
-        <button type="button" style={buttonStyle} className="ms-5 mt-5 d-flex align-items-center">
+      <div className="ms-auto me-4 mt-3">
+        <a href="/" className="text-dark me-3">イベント主催者はこちら</a>
+        <button type="button" style={buttonStyle} className="mt-5 ms-auto d-flex align-items-center">
           <span>メニュー</span>
           <h2 className="ms-2"><HiOutlineMenu /></h2>
         </button>
