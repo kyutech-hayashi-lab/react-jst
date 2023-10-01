c4f5f6d86d85d90c69fdf0774d14ac26181bbae8
takemoto-eye-sales <t.mizuki@x-opg.com>
2023-10-01 18:07:44 +0900

    イベントフォームの体裁を整える

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,7 +11,7 @@ export default function Header() {
     <div className="d-flex px-2 border-bottom border-2 position-fixed w-100 bg-white top-0">
       <h3 className="mt-4 ms-5"><strong>ビーチクリーンクラウドサービス</strong></h3>
       <div className="ms-auto me-4 mt-3">
-        <a href="/" className="text-dark me-3">イベント主催者はこちら</a>
+        <a href="/form" className="text-dark me-3">イベント主催者はこちら</a>
         <button type="button" style={buttonStyle} className="mt-5 ms-auto d-flex align-items-center">
           <span>メニュー</span>
           <h2 className="ms-2"><HiOutlineMenu /></h2>
--- src/pages/EventFormPage.tsx
+++ src/pages/EventFormPage.tsx
@@ -1,19 +1,10 @@
 import React from 'react';
-import { init, send } from '@emailjs/browser';
-import { Button } from 'react-bootstrap';
 import EventForm from '../components/EventForm';
 
 export default function EventFormPage() {
-  init('q5ScCMTEE8dwqodJ5');
-  const click = () => {
-    send('service_3k42d1k', 'template_ho5cp9v', { from_name: 'test',
-      message: 'hoge' }).catch((err) => { throw err; });
-  };
-
   return (
     <div>
       <EventForm />
-      <Button onClick={() => click()}>テスト</Button>
     </div>
   );
 }
